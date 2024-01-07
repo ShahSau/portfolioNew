f4243202cfadd955dbf5c9dbc39a0fdc1ba19eae
shahriar <shahriarksaurov@gmail.com>
2024-01-07 11:48:37 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function home() {
     <>
       <Head>
         <title>Shahriar | Web Developer</title>
-        <meta name="description" content="" />
+        <meta name="description" content="Portfolio of Shahriar Karim Saurov. An experienced fullstack developer" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
