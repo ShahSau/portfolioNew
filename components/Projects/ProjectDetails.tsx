d04142785fe5087975d8f05f31fd91b37980db04
shahriar <shahriarksaurov@gmail.com>
2024-12-08 00:38:43 +0100

    project details (WIP)

---
+++ components/Projects/ProjectDetails.tsx
@@ -0,0 +1,113 @@
+import {
+    useDragControls,
+    useMotionValue,
+    useAnimate,
+    motion,
+} from "framer-motion";
+import React, {  Dispatch, FC, ReactNode, SetStateAction,  } from "react";
+import useMeasure from "react-use-measure";
+
+interface Props {
+    open: boolean;
+    setOpen: Dispatch<SetStateAction<boolean>>;
+    children?: ReactNode;
+}
+
+const ProjectDetails: FC<Props> = ({ open, setOpen, children }) => {
+    const [scope, animate] = useAnimate();
+    const [drawerRef, { height }] = useMeasure();
+  
+    const y = useMotionValue(0);
+    const controls = useDragControls();
+  
+    const handleClose = async (): Promise<void> => {
+      animate(scope.current, {
+        opacity: [1, 0],
+      });
+  
+      const yStart = typeof y.get() === "number" ? y.get() : 0;
+  
+      await animate("#drawer", {
+        y: [yStart, height],
+      });
+  
+      setOpen(false);
+    };
+  
+    return (
+      <>
+        {open && (
+          <motion.div
+            ref={scope}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            onClick={handleClose}
+            className="fixed inset-0 z-50 bg-neutral-950/70"
+          >
+            <motion.div
+              id="drawer"
+              ref={drawerRef}
+              onClick={(e) => e.stopPropagation()}
+              initial={{ y: "100%" }}
+              animate={{ y: "0%" }}
+              transition={{
+                ease: "easeInOut",
+              }}
+              className="absolute bottom-0 h-[75vh] w-full overflow-hidden rounded-t-3xl bg-gray-300"
+              style={{ y }}
+              drag="y"
+              dragControls={controls}
+              onDragEnd={() => {
+                if (y.get() >= 100) {
+                  handleClose();
+                }
+              }}
+              dragListener={false}
+              dragConstraints={{
+                top: 0,
+                bottom: 0,
+              }}
+              dragElastic={{
+                top: 0,
+                bottom: 0.5,
+              }}
+            >
+              <div className="absolute left-0 right-0 top-0 z-10 flex justify-center bg-neutral-900 p-4">
+                <button
+                  onPointerDown={(e) => {
+                    controls.start(e);
+                  }}
+                  className="h-2 w-14 cursor-grab touch-none rounded-full bg-neutral-700 active:cursor-grabbing"
+                ></button>
+                {/**Close button */}
+                <button
+                  onClick={handleClose}
+                  className="absolute right-4 top-0 p-2 rounded-full bg-neutral-900"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 text-neutral-200"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div className="relative z-0 h-full overflow-y-scroll p-4 pt-12">
+                {children}
+              </div>
+            </motion.div>
+          </motion.div>
+        )}
+      </>
+    );
+  };
+
+export default ProjectDetails;
