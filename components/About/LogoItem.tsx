fa268a7eaccff05180400ca8590e78739c4b0268
shahriar <shahriarksaurov@gmail.com>
2025-04-27 11:04:19 +0200

    small update

--- components/About/LogoItem.tsx
+++ components/About/LogoItem.tsx
@@ -1,14 +1,29 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { IconType } from "react-icons";
+
+export const IconToTextMap = (Icon: IconType) => {
+  return Icon.name
+    .replace("Si", "")
+    .replace(/([A-Z])/g, " $1")
+    .trim();
+};
 
 interface LogoItemProps {
   Icon: IconType;
+  Text: string;
 }
 
-const LogoItem: FC<LogoItemProps> = ({ Icon }) => {
+const LogoItem: FC<LogoItemProps> = ({ Icon, Text }) => {
+  const [isHovered, setIsHovered] = useState(false);
   return (
-    <span className="w-full  h-16 md:h-24 flex justify-center items-center hover:bg-slate-200 text-black transition-colors">
+    <span className="w-full  h-16 md:h-24 flex justify-center items-center text-black transition-colors"
+    onMouseEnter={() => setIsHovered(true)}
+    onMouseLeave={() => setIsHovered(false)}
+    >
       <Icon className="text-4xl md:text-5xl" />
+      {isHovered && (
+        <p className="hidden md:block absolute mt-16 text-sm">{Text}</p>
+      )}
     </span>
   );
 };
--- components/About/LogoItemsBottom.tsx
+++ components/About/LogoItemsBottom.tsx
@@ -1,20 +1,4 @@
 import {
-  SiJavascript,
-  SiGo,
-  SiReact,
-  SiAngular,
-  SiNodedotjs,
-  SiExpress,
-  SiNestjs,
-  SiNextui,
-  SiNextdotjs,
-  SiDjango,
-  SiMongodb,
-  SiPostgresql,
-  SiGin,
-  SiTypescript,
-  SiPython, 
-  SiGraphql,
   SiPrisma,
   SiRedis,
   SiApachekafka,
@@ -30,29 +14,35 @@ import {
   SiMaterialdesignicons,
   SiMaterialdesign,
   SiHtml5,
-  SiCss3
+  SiCss3,
 } from "react-icons/si";
-import LogoItem from "./LogoItem";
 import { FC } from "react";
 import { TbBrandReactNative } from "react-icons/tb";
+import LogoItem, { IconToTextMap } from "./LogoItem";
+
 const LogoItemsBottom: FC = () => (
   <>
-    <LogoItem Icon={SiPrisma} />
-    <LogoItem Icon={SiRedis} />
-    <LogoItem Icon={SiApachekafka} />
-    <LogoItem Icon={SiTerraform} />
-    <LogoItem Icon={SiKubernetes} />
-    <LogoItem Icon={SiCypress} />
-    <LogoItem Icon={SiJest} />
-    <LogoItem Icon={SiAmazonwebservices} />
-    <LogoItem Icon={SiGooglecloud} />
-    <LogoItem Icon={SiRedux} />
-    <LogoItem Icon={SiNgrx} />
-    <LogoItem Icon={SiTailwindcss} />
-    <LogoItem Icon={SiMaterialdesign} />
-    <LogoItem Icon={SiHtml5} />
-    <LogoItem Icon={TbBrandReactNative} />
-    <LogoItem Icon={SiCss3} />
+    {[
+      SiPrisma,
+      SiRedis,
+      SiApachekafka,
+      SiTerraform,
+      SiKubernetes,
+      SiCypress,
+      SiJest,
+      SiAmazonwebservices,
+      SiGooglecloud,
+      SiRedux,
+      SiNgrx,
+      SiTailwindcss,
+      SiMaterialdesignicons,
+      SiMaterialdesign,
+      SiHtml5,
+      TbBrandReactNative,
+      SiCss3,
+    ].map((Icon, index) => (
+      <LogoItem key={index} Icon={Icon} Text={IconToTextMap(Icon)} />
+    ))}
   </>
 );
 
